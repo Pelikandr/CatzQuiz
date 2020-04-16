d919a53200aa94bad53442fe2ee11993e8ac32aa
pelikandr <denis.zayakin@gmail.com>
2020-04-16 15:43:49 +0300

    initial

---
+++ CatzQuiz/Network.swift
@@ -0,0 +1,13 @@
+//
+//  Network.swift
+//  CatzQuiz
+//
+//  Created by pelikandr on 16/04/2020.
+//  Copyright © 2020 pelikandr. All rights reserved.
+//
+
+import Foundation
+
+class Network {
+    
+}
---
+++ CatzQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatzQuiz
+//
+//  Created by pelikandr on 15/04/2020.
+//  Copyright © 2020 pelikandr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
